6a5a41d496bd77a928f488006a352c1e9922e2bc
Nitin1604 <nitinsokhal@gmail.com>
2023-04-10 00:01:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("Welcome to Spotify Clone");
+// audioElement.play(); --> It will play the audio
+
+// Intialize the variables
+let songIndex = 0;
+
+// ----- Playing the songs by javascript ----- 
+let audioElement = new Audio('songs/song1.mp3');
+// ----- Playing the songs by javascript ----- 
+
+// masterPlay is the id of play button
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+// let masterSongName = document.getElementById('masterSongName');
+// let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Salam-e-Ishq", filePath: "songs/song1.mp3", coverPath: "covers/cover1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/song2.mp3", coverPath: "covers/cover2.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/song3.mp3", coverPath: "covers/cover3.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/song4.mp3", coverPath: "covers/cover4.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/song5.mp3", coverPath: "covers/cover5.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/song6.mp3", coverPath: "covers/cover6.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/song7.mp3", coverPath: "covers/cover7.jpg"}
+] 
+
+// Handle play/ pause click
+masterPlay.addEventListener('click', ()=>{
+     if(audioElement.paused || audioElement.currentTime<=0) {
+        audioElement.play();
+         masterPlay.classList.remove('fa-play')
+         masterPlay.classList.add('fa-pause')
+         gif.style.opacity = 1;
+        }
+        else {
+            audioElement.pause();
+            masterPlay.classList.remove('fa-pause')
+            masterPlay.classList.add('fa-play')
+            gif.style.opacity = 0;
+
+        }
+       
+})
+
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate')
+    //  Update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+// Change event in myprogressBar id
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+}) 
